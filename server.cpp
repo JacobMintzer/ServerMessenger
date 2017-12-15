32ff1abf3a9413bbb1ca3e326d4885bec71e65db
Ubuntu <ubuntu@ip-172-31-24-168.us-west-2.compute.internal>
2017-12-15 06:38:52 +0000

    software works, successfully reads error file and sends it through tcp to client (me)

--- server.cpp
+++ server.cpp
@@ -8,17 +8,17 @@
 #include <iomanip>
 #include <cstring>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-std::string checkError(char* errlog){
-	errlog++;
-	std::ifstream in_str("../Twitter/Statistics/twitter_errlog.txt");
+std::string checkError(){
+	std::ifstream in_str("../Twitter-Statistics/twitter_errlog.txt");
 
 	std::string errorMessage="";
 	if (!in_str.good()){
-		std::cerr<<"can't open file"<<errlog<<std::endl;
+		std::cerr<<"can't open file"<<std::endl;
 	}
 	else{
 		std::stringstream buffer;
@@ -26,7 +26,7 @@ std::string checkError(char* errlog){
 		errorMessage=buffer.str();
 		in_str.close();
 		std::ofstream ofs;
-		ofs.open(errlog, std::ofstream::out | std::ofstream::trunc);
+		ofs.open("../Twitter-Statistics/twitter_errlog.txt", std::ofstream::out | std::ofstream::trunc);
 		ofs.close();
 		if (errorMessage.length()>3){
 			return errorMessage;
@@ -35,14 +35,21 @@ std::string checkError(char* errlog){
 	return "";
 }
 
+long getFileSize(){
+    struct stat stat_buf;
+    int rc = stat("../Twitter-Statistics/twitter_errlog.txt", &stat_buf);
+    return rc == 0 ? stat_buf.st_size : -1;
+}
+
 int main(int argc, char* argv[]){
-	/*std::string ip;
-	std::string port;
-	std::ifstream readIP("ip.txt");
-	readIP>>port;
-	readIP.close();
-*/
-	
+	/*
+	 *std::string ip;
+	 *std::string port;
+	 *std::ifstream readIP("ip.txt");
+	 *readIP>>port;
+	 *readIP.close();
+	 */
+
 	int sd = socket( PF_INET, SOCK_STREAM, 0 );
 	struct sockaddr_in server;
 	server.sin_family = PF_INET;
@@ -61,27 +68,34 @@ int main(int argc, char* argv[]){
 	struct sockaddr_in client;
 	int fromlen = sizeof( client );
 	int n;
-	
-	
+
+
 	std::string error;
-	int newsd;
+	int newsd= accept( sd, (struct sockaddr *)&client, (socklen_t *)&fromlen );
 	while (true){
-		error=checkError(argv[1]);
-		if(error.empty()){
-			usleep(30000);
+		if(getFileSize()<3){
+			sleep(300);
 		}
 		else{
-			newsd = accept( sd, (struct sockaddr *)&client, (socklen_t *)&fromlen );
+			error=checkError();
+
+			//std::cout<<"blocking on send"<<std::endl;
 			n=send(newsd,error.c_str(),error.length(),0);
 			if ( n == -1 ){
-			    std::cerr<< "read() failed";
-    			return EXIT_FAILURE;
+				std::cerr<< "read() failed";
+    				return EXIT_FAILURE;
   			}
   			else if ( n == 0 ){
- 		   		std::cout<<"socket was closed"<<std::endl;
+				while (n<1){
+	 		   		std::cout<<"socket was closed, trying again"<<std::endl;
+					newsd= accept( sd, (struct sockaddr *)&client, (socklen_t *)&fromlen );
+					n=send(newsd,error.c_str(),error.length(),0);
+					sleep(120);
+				}
+
   			}
   			else{
-  				std::cout<<"probably sent"<<std::endl;
+  				std::cout<<"sent"<<std::endl;
   			}
 		}
 	}
